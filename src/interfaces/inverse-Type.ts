1bf68e86b4772c6e04cdb6112f36e67de87aeb9c
TikhampornSky <tikhamporntan@gmail.com>
2022-11-09 12:20:11 +0700

    Little Inversufy

--- src/interfaces/inverse-Type.ts
+++ src/interfaces/inverse-Type.ts
@@ -1,7 +1,11 @@
 let TYPES = {
     Warrior: Symbol("Warrior"),
     Weapon: Symbol("Weapon"),
-    ThrowableWeapon: Symbol("ThrowableWeapon")
+    ThrowableWeapon: Symbol("ThrowableWeapon"),
+    GetService: Symbol("GetService"),
+    PostService: Symbol("PostService"),
+    UpdateService: Symbol("UpdateService"),
+    DeleteService: Symbol("DeleteService"),
 };
 
 export default TYPES
--- src/services/DIContainer.ts
+++ src/services/DIContainer.ts
@@ -4,14 +4,20 @@ import { Ninja, Katana, Shuriken } from "./inverse-Class";
 import Warrior from '../interfaces/inverse-Warrior';
 import Weapon from '../interfaces/inverse-Weapon';
 import ThrowableWeapon from '../interfaces/inverse-ThrowableWeapon'
-import Post from "../interfaces/Post";
+
 import GetService from "./GetService";
+import PostService from "./PostService";
+import UpdateService from "./UpdateService";
+import DeleteService from "./DeleteService";
 
 var container = new Container();
 container.bind<Warrior>(TYPES.Warrior).to(Ninja);
 container.bind<Weapon>(TYPES.Weapon).to(Katana);
 container.bind<ThrowableWeapon>(TYPES.ThrowableWeapon).to(Shuriken);
 
-// container.bind(A).to(B)
+container.bind<GetService>(TYPES.GetService).to(GetService);
+container.bind<PostService>(TYPES.PostService).to(PostService);
+container.bind<UpdateService>(TYPES.UpdateService).to(UpdateService);
+container.bind<DeleteService>(TYPES.DeleteService).to(DeleteService);
 
 export default container;
